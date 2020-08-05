b3771f83d6e38e016be3a62c4582cb038692946d
xiemnx <xiem.bg@gmail.com>
2020-08-05 14:45:21 +0700

    html/css

---
+++ scss/config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "../css"
+sass_dir = ""
+images_dir = "../images"
+javascripts_dir = "../js"
+output_style = :expanded
+relative_assets = true
+line_comments = false
+
+cache_dir = "../../.sass-cache" #キャッシュファイルの場所
+# cache = false #キャッシュファイル作成の無効
